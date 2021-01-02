25233cfa358c569f2ece9672337ab103489f52bf
Curtis Pritchard <curtis.pritchard@stclaircconnect.ca>
2021-01-02 16:37:53 -0500

    made the portfolio look decent on phones

--- script.js
+++ script.js
@@ -2,31 +2,33 @@
 
 let h1 = "100px";
 
-window.onscroll = function () {
-   
-    let scrollTop = document.body.scrollTop;
-    let elmScrollTop = document.documentElement.scrollTop;
-    // console.log(scrollTop + " - " + elmScrollTop)
-    
-    if (scrollTop > 150 || elmScrollTop > 150) {
-        document.querySelector(".stuffer").style.maxHeight = "0"
-        document.querySelector("h1").style.fontSize = "30px";
-        document.querySelector("header h1 ~ div div").style.maxHeight = "0";
-        document.querySelectorAll("header h1 ~ div div").forEach( elm => {
-            elm.style.maxHeight = "0"; 
-            elm.style.position = "absolute";
-    
-    });
 
-    
-    } else {
+window.onscroll = function () {
+    if (screen.width > 500) {
+        let scrollTop = document.body.scrollTop;
+        let elmScrollTop = document.documentElement.scrollTop;
+        // console.log(scrollTop + " - " + elmScrollTop)
         
-        document.querySelector('.stuffer').style.maxHeight = "calc(100vh / 3 - " + h1
-        document.querySelector("h1").style.fontSize = "100px"
-        document.querySelectorAll("header h1 ~ div div").forEach( elm => {
-           elm.style.maxHeight = "60px"; 
-           elm.style.position = "relative";
+        if (scrollTop > 150 || elmScrollTop > 150) {
+            document.querySelector(".stuffer").style.maxHeight = "0"
+            document.querySelector("h1").style.fontSize = "30px";
+            document.querySelector("header h1 ~ div div").style.maxHeight = "0";
+            document.querySelectorAll("header h1 ~ div div").forEach( elm => {
+                elm.style.maxHeight = "0"; 
+                elm.style.position = "absolute";
+        
         });
 
-    }
+        
+        } else {
+            
+            document.querySelector('.stuffer').style.maxHeight = "calc(100vh / 3 - " + h1
+            document.querySelector("h1").style.fontSize = "100px"
+            document.querySelectorAll("header h1 ~ div div").forEach( elm => {
+            elm.style.maxHeight = "60px"; 
+            elm.style.position = "relative";
+            });
+
+        }
+    }   
 }
